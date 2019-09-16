ac74b96c0e628139c56835c7142183d4dfba59ba
lubin <lubin@wecash.net>
2019-09-16 16:29:38 +0800

    letcode 整数反转 注释添加

--- stringReverse.go
+++ stringReverse.go
@@ -15,16 +15,16 @@ func reverse(x int) int {
 	if x >= 0 && x < 10 {
 		return x
 	}
-	xStr := strconv.Itoa(x) //x转换成字符串
-	var INT_MIN int64 = -2147483648
-	var INT_MAX int64 = 2147483647
+	xStr := strconv.Itoa(x)         //x转换成字符串
+	var INT_MIN int64 = -2147483648 //定义int最小值
+	var INT_MAX int64 = 2147483647  //定义int最大值
 	var y int64 = 0
-	if strings.HasPrefix(xStr, "-") {
-		xNew := strings.Replace(xStr, "-", "", 1)
-		xDao := reverseString(xNew)
+	if strings.HasPrefix(xStr, "-") { //判断整数前缀是否是负数
+		xNew := strings.Replace(xStr, "-", "", 1) //去掉负号
+		xDao := reverseString(xNew)               //反转新的整数
 		xDao = "-" + xDao
 		y, _ = strconv.ParseInt(xDao, 10, 64)
-		if y < INT_MIN || y > INT_MAX {
+		if y < INT_MIN || y > INT_MAX { //判断是否在int范围之内
 			return 0
 		}
 	} else {
@@ -34,7 +34,7 @@ func reverse(x int) int {
 			return 0
 		}
 	}
-	yStr := strconv.FormatInt(y, 10)
+	yStr := strconv.FormatInt(y, 10) //字符串转整数
 	result, _ := strconv.Atoi(yStr)
 	return result
 
