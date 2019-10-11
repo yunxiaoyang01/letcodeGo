09a692edd97cd1eff9db1d7ecd43c046f9fc8b95
lubin <lubin@wecash.net>
2019-10-11 15:26:22 +0800

    leetcode 最后一个单词的长度

---
+++ LengthOfLastWord.go
@@ -0,0 +1,18 @@
+package main
+
+import "strings"
+
+func main() {
+	println(lengthOfLastWord("a "))
+}
+
+func lengthOfLastWord(s string) int {
+	slen := 0
+	s1 := strings.TrimSpace(s)
+	sArray := strings.Split(s1, " ")
+	length := len(sArray)
+	if length != 0 {
+		slen = len(sArray[length-1])
+	}
+	return slen
+}
