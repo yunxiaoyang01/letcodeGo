67b715575b2563b47b1e685a756989b2dcfeb460
lubin <lubin@wecash.net>
2019-10-17 17:47:33 +0800

    leetcode Pow(x, n)

---
+++ myPow.go
@@ -0,0 +1,34 @@
+package main
+
+func main() {
+
+	println(myPow1(2, 10))
+}
+
+func myPow(x float64, n int) float64 {
+	if n == 0 {
+		return 1
+	}
+	if n < 0 {
+		return 1 / myPow(x, -n)
+	}
+	if n%2 != 0 {
+		return x * myPow(x, n-1)
+	}
+	return myPow(x*x, n/2)
+}
+
+//快速幂
+func myPow1(x float64, n int) float64 {
+	res := 1.0
+	for i := n; i != 0; i /= 2 {
+		if i%2 != 0 {
+			res *= x
+		}
+		x *= x
+	}
+	if n < 0 {
+		return 1.0 / res
+	}
+	return res
+}
