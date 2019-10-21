20304e1aa8a84893f941f6141eabfa8c88046f0d
lubin <lubin@wecash.net>
2019-10-21 17:45:12 +0800

    leetcode Excel表列序号

---
+++ convertToTitle.go
@@ -0,0 +1,43 @@
+package main
+
+func main() {
+	println(convertToTitle(52))
+}
+
+func convertToTitle(n int) string {
+	cons := map[int]string{
+		1:  "A",
+		2:  "B",
+		3:  "C",
+		4:  "D",
+		5:  "E",
+		6:  "F",
+		7:  "G",
+		8:  "H",
+		9:  "I",
+		10: "J",
+		11: "K",
+		12: "L",
+		13: "M",
+		14: "N",
+		15: "O",
+		16: "P",
+		17: "Q",
+		18: "R",
+		19: "S",
+		20: "T",
+		21: "U",
+		22: "V",
+		23: "W",
+		24: "X",
+		25: "Y",
+		26: "Z",
+	}
+	result := ""
+	for n != 0 {
+		t := (n - 1) % 26
+		result = cons[t+1] + result
+		n = (n - 1) / 26
+	}
+	return result
+}
