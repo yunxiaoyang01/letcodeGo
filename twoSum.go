f684dc291e74877c69c356c96befc3cf159ce274
lubin <lubin@wecash.net>
2019-10-21 17:04:23 +0800

    leetcode 两数之和 II - 输入有序数组

---
+++ twoSum.go
@@ -0,0 +1,26 @@
+package main
+
+func main() {
+	numbers := []int{2, 7, 11, 15}
+	target := 9
+	for _, x := range twoSum(numbers, target) {
+		println(x)
+	}
+}
+
+func twoSum(numbers []int, target int) []int {
+
+	l, r := 0, len(numbers)-1
+	for l < r {
+		sum := numbers[l] + numbers[r]
+		if sum < target {
+			l++
+		} else if sum > target {
+			r--
+		} else {
+			break
+		}
+	}
+	result := []int{l + 1, r + 1}
+	return result
+}
