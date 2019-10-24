f6d77eae8a5c80f1fccc1cbccec15f009b8c935d
lubin <lubin@wecash.net>
2019-10-24 15:42:00 +0800

    leetcode 2的幂

---
+++ isPowerOfTwo.go
@@ -0,0 +1,14 @@
+package main
+
+func main() {
+	println(isPowerOfTwo(2))
+}
+func isPowerOfTwo(n int) bool {
+	if n == 0 {
+		return false
+	}
+	if n&(n-1) == 0 {
+		return true
+	}
+	return false
+}
