7512d841bdf3217405a81a1590b12e41b9b87cfb
lubin <lubin@xiaoduotech.com>
2021-05-25 09:51:48 +0800

    相交链表

---
+++ easy/160. 相交链表/getIntersectionNode.go
@@ -0,0 +1,31 @@
+package main
+
+func main() {
+
+}
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func getIntersectionNode(headA, headB *ListNode) *ListNode {
+	if headA == nil || headB == nil {
+		return nil
+	}
+	head1 := headA
+	head2 := headB
+	for head1 != head2 {
+		if head1 !=nil {
+			head1 = head1.Next
+		}else{
+			head1 = headB
+		}
+		if head2 != nil {
+			head2 = head2.Next
+		}else{
+			head2 = headA
+		}
+	}
+	return head1
+}
