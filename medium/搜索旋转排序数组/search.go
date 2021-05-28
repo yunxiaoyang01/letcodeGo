6485988dc7552c86c098f779e760a0635b2d78e3
lubin <lubin@xiaoduotech.com>
2021-05-28 10:12:21 +0800

    搜索旋转排序数组

--- medium/搜索旋转排序数组/search.go
+++ medium/搜索旋转排序数组/search.go
@@ -4,42 +4,46 @@ import (
 	"fmt"
 )
 
-func search(nums []int, target int) bool {
+func search(nums []int, target int) int {
 	n := len(nums)
-	if n==0{
-		return false
+	if n == 0 {
+		return -1
 	}
-	if n==1{
-		return nums[0]==target
-	}
-	l,r :=0,n-1
-	for l<=r{
-		mid := (l+r)/2
-		if nums[mid]==target {
-			return true
+	if n == 1 {
+		if target == nums[0] {
+			return 0
+		} else {
+			return -1
 		}
-		if nums[l]==nums[mid] && nums[r]==nums[mid]{
+	}
+	l, r := 0, n-1
+	for l <= r {
+		mid := (l + r) / 2
+		if nums[mid] == target {
+			return mid
+		}
+		if nums[mid] == nums[l] && nums[mid] == nums[r] {
 			l++
 			r--
-		}else if nums[l] <= nums[mid] {
-			if nums[l]<= target && target < nums[mid]{
-				r = mid-1
-			}else{
-				l = mid+1
+		} else if nums[l] <= nums[mid] {
+			if nums[l] < target && target < nums[mid] {
+				r = mid - 1
+			} else {
+				l = mid + 1
 			}
-		}else{
-			if nums[mid]<target && target <=nums[n-1]{
-				l = mid+1
-			}else{
-				r = mid-1
+		} else {
+			if nums[mid] < target && target < nums[n-1] {
+				l = mid + 1
+			} else {
+				r = mid - 1
 			}
 		}
 	}
-	return false
+	return -1
 }
 
 func main() {
-	nums := []int{4,5,6,6,7,0,1,2,4,4}
+	nums := []int{4, 5, 6, 7, 0, 1, 2}
 	target := 2
-	fmt.Println(search(nums,target))
+	fmt.Println(search(nums, target))
 }
